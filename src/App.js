fc1d7afa5f9ac1b77d6c73fb837bd93f22acf8d6
Balanagu Harsha Vardhan <bavardha@microsoft.com>
2020-08-09 14:15:53 +0530

    Basic connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from './axios';
 
 function App() {
+  let [res,setRes] = useState("")
+  axios.get(`/`).then(data=>{
+    console.log(data.data)
+    setRes(data.data)
+  })
+      
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {res}
         </p>
         <a
           className="App-link"
