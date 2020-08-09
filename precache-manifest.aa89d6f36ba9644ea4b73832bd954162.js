28a8b6e64f63753e8adf887075f4f68e0151173d
Balanagu Harsha Vardhan <bavardha@microsoft.com>
2020-08-09 14:25:09 +0530

    Updates

---
+++ precache-manifest.aa89d6f36ba9644ea4b73832bd954162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60349145035b65324761fd98501ff479",
+    "url": "/DlogAssistant/index.html"
+  },
+  {
+    "revision": "d4321dfe1cabf68d7227",
+    "url": "/DlogAssistant/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee4dd39cb75026daf6a6",
+    "url": "/DlogAssistant/static/js/2.a8ed0690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DlogAssistant/static/js/2.a8ed0690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4321dfe1cabf68d7227",
+    "url": "/DlogAssistant/static/js/main.f76dfbf1.chunk.js"
+  },
+  {
+    "revision": "f04c05033778f1127ac0",
+    "url": "/DlogAssistant/static/js/runtime-main.4c6ec2a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DlogAssistant/static/media/logo.5d5d9eef.svg"
+  }
+]);
